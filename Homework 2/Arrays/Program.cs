9f762e1c109dcd483313dd1925c4f665d57372dc
Ivan Mitev <ivan.mitev90@gmail.com>
2020-03-14 18:35:19 +0100

    Homework 2

---
+++ Homework 2/Arrays/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] allNumbers = new int[6];
+            
+            Console.WriteLine("Enter Integer no.1");
+             allNumbers[0] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Integer no.2");
+             allNumbers[1] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Integer no.3");
+             allNumbers[2] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Integer no.4");
+             allNumbers[3] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Integer no.5");
+             allNumbers[4] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Integer no.6");
+             allNumbers[5] = int.Parse(Console.ReadLine());
+            
+            int sum = 0;
+            foreach (int number in allNumbers)
+            {
+                if (number % 2 == 0)
+                {
+                    sum += number;
+                };
+               
+            }
+            Console.WriteLine();
+            Console.WriteLine("The result is: " + sum);
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ Homework 2/Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select a student group. 1 or 2");
+
+            int studentGroup = int.Parse(Console.ReadLine());
+            
+            string[] g1 = {"Zdravko", "Petko", "Stanko", "Branko", "Trajko"};
+            string[] g2 = { "Igor", "Dejan", "Martin", "Ivan", "Davor" };
+
+
+            switch (studentGroup)
+            {
+                case 1: 
+                    foreach(string student in g1)
+                    {
+                        Console.WriteLine(student);
+                    }
+                    break;
+
+                case 2:
+                    foreach(string student in g2)
+                    {
+                        Console.WriteLine(student);
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter the right group");
+                    break;
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
