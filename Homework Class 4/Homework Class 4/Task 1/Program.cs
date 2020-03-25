584257e476377cbca4aba54ff24cd21ec0261749
Ivan Mitev <ivan.mitev90@gmail.com>
2020-03-25 20:29:25 +0100

    Homework class 4

---
+++ Homework Class 4/Homework Class 4/Task 1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Substrings();
+                Console.ReadLine();
+            }
+        }
+
+        static void Substrings()
+        {
+
+            string hello = "Hello from SEDC Codecademy v7.0";
+            Console.WriteLine(hello);
+            Console.WriteLine($"Please enter a number between 1 and {hello.Length}");
+            
+            bool parseInput = int.TryParse(Console.ReadLine(), out int input);
+            if (!parseInput || input < 1 || input > hello.Length )
+            {
+                Console.WriteLine("Wrong input");
+            }
+            else
+            {
+                char[] characters = hello.ToCharArray();
+                Console.WriteLine($@"The character ""{characters[input - 1]}"" is at place {input}");
+
+                string substring = hello.Substring(0, input);
+                Console.WriteLine($@"The first {input} characters are ""{substring}"" and the length of this substring is {substring.Length}");
+            }
+            
+        }
+    }
+}
---
+++ Homework Class 4/Homework Class 4/Task 2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter - or +");
+                string plusOrMinus = Console.ReadLine();
+
+                if (plusOrMinus == "+" || plusOrMinus == "-")
+                {
+                    Console.WriteLine("Please enter the first number");
+                    bool parseNumA = double.TryParse(Console.ReadLine(), out double numA);
+
+                    if (!parseNumA)
+                    {
+                        Console.WriteLine("Wrong input must enter a number. Press any key to start again");
+                        Console.ReadLine();
+                        Console.Clear();
+                        continue;
+                    }
+
+                    Console.WriteLine("Please enter the second number");
+                    bool parseNumB = double.TryParse(Console.ReadLine(), out double numB);
+
+                    if (!parseNumB)
+                    {
+                        Console.WriteLine("Wrong input must enter a number. Press any key to start again");
+                        Console.ReadLine();
+                        Console.Clear();
+                        continue;
+                    }
+
+                    if (plusOrMinus == "+")
+                    {
+                        Console.WriteLine(Sum(numA, numB));
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+                    else if (plusOrMinus == "-")
+                    {
+                        Console.WriteLine(Subtract(numA, numB));
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input must enter - of +. Press any key to start again");
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                } 
+            }
+        }
+        static string Sum(double numA, double numB)
+        {
+            double sum = numA + numB;
+            return $"The sum of {numA} and {numB} is {sum}";
+        }
+        static string Subtract(double numA, double numB)
+        {
+            double subtract = numA - numB;
+            return $"The subtract of {numA} and {numB} is {subtract}";
+        }
+    }
+}
